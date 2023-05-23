781414e34863a7025a87a18d481c150ef50aa365
Yuliia Kulachko <svistaky@gmail.com>
2023-05-23 12:54:18 +0300

    Updated get data from backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import MainPage from "./components/Main/MainPage";
 function App() {
   const [data, setData] = useState();
   useEffect(()=>{
-    fetch("https://morent-car-rent.onrender.com/")
+    fetch("https://morent-car-rent.onrender.com/cars")
     .then((response) => response.json())
     .then(response=>setData(response))
     .catch((error) => {
