26f6e22c15b8129adbb9e901e0a7cde697d3da85
Yuliia Kulachko <svistaky@gmail.com>
2023-05-23 15:57:19 +0300

    remove video  autoplay in full screen

--- frontend/src/components/Main/About/Featured.js
+++ frontend/src/components/Main/About/Featured.js
@@ -7,7 +7,7 @@ export default function Featured(props){
         <h2>{props.title}</h2>
         <p>Our Popular cars section showcases some of the best cars in our fleet. These cars are handpicked by our team for their exceptional performance, style, and features.</p>
         <div>
-        <video autoPlay muted loop>
+        <video autoPlay muted loop playsInline>
             <source src={video} type="video/mp4"/>
         </video>
         </div>
